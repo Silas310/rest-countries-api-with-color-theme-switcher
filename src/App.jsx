4c9559fe478a7743076ad89a71a561c6079cde0a
Silas <silascosta310@gmail.com>
2025-04-15 19:11:55 -0300

    feat: add search input

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import "../src/index.css"
 import Header from "../src/components/Header"
+import Search from "./components/Search"  
 
 function App() {
 
   return (
     <div className="flex justify-center w-screen h-screen">
-      <main className="text-(length:--font-size-home) flex flex-col w-full">
+      <main className="text-(length:--font-size-home) flex flex-col w-full bg-light-bg">
         <Header />
+        <Search />
       </main>
     </div>
   )
