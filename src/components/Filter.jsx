68281a7d48b5cfbcd90dc76155b369110a6c6cf4
Silas <silascosta310@gmail.com>
2025-04-17 17:32:04 -0300

    feat: toggle region filter visibility on label click

--- src/components/Filter.jsx
+++ src/components/Filter.jsx
@@ -1,8 +1,22 @@
+import { useState } from "react"
+
+
 function Filter() {
+  let [visibility, setVisibility] = useState("hidden")
+
+
+  function handleVisibility() {
+    if (visibility === "hidden") {
+      setVisibility("block")
+    } else {
+      setVisibility("hidden")
+    }
+  }
+  
   return (
     <div>
-      <label htmlFor="region" className="bg-white py-4 border">Filter by region</label>
-      <select name="region" className="hidden">
+      <label htmlFor="region" className="bg-white py-4 border" onClick={handleVisibility}>Filter by region</label>
+      <select name="region" className={visibility}>
         <option value="africa">Africa</option>
         <option value="america">America</option>
         <option value="asia">Asia</option>
