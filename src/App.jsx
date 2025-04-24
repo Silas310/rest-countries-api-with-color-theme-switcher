dd0bc510a08f4ec982230941878598aef5b870a8
Silas <silascosta310@gmail.com>
2025-04-24 07:07:14 -0300

    feat: fetch countries data

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,16 @@ import "../src/index.css"
 import Header from "./components/Header"
 import Form from "./components/Form"
 import CountriesList from "./components/CountriesList"  
+import useSWR from "swr"
+
+
+const fetcher = (...args) => fetch(...args).then(res => res.json())
 
 
 function App() {
+  const { data, error, isLoading } = useSWR("https://restcountries.com/v3.1/alpha?codes=DE,US,BR,IS,AF,AX,AL,DZ", fetcher)
+  console.log(data);
+  
 
   return (
     <div className="flex justify-center w-screen h-screen">
