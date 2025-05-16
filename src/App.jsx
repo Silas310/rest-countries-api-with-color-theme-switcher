96eeea4f84643dee050057444d72e28321b0f0e9
Silas <silascosta310@gmail.com>
2025-05-16 16:11:52 -0300

    fix: blank page after search invalid country

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,14 @@ import { useState } from "react"
 import useSWR from "swr"
 
 
-const fetcher = (...args) => fetch(...args).then(res => res.json())
+// const fetcher = (...args) => fetch(...args).then(res => res.json())
+
+const fetcher = async (...args) => {
+  const res = await fetch(...args);
+  if (!res.ok) throw new Error('Erro ao buscar países');
+  return res.json();
+}
+
 
 
 function App() {
