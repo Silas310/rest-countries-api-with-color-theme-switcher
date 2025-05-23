5145cf11e5944158565a4359bcbb315cd5e21f3e
Silas <silascosta310@gmail.com>
2025-05-23 20:10:31 -0300

    feat: turn Card a Link component

--- src/components/CountriesList.jsx
+++ src/components/CountriesList.jsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom';
 import Card from './Card';
 import Loading from './Loading';
 import ErrorAlert from './ErrorAlert';
@@ -12,15 +13,18 @@ function CountriesList({ data, error, isLoading }) {
   return (
     <section className='flex flex-col items-center gap-y-10 dark:bg-dark-bg'>
       {data.map((country) => (
-        <Card
-          key={country.cca3}
-          flag={country.flags.png}
-          altFlag={country.flags.alt}
-          name={country.name.common}
-          population={country.population.toLocaleString()}
-          region={country.region}
-          capital={country.capital ? country.capital[0] : "No capital"}
-        />
+        <Link key={country.cca3} to={`/country/${country.cca3}`}>
+          <Card
+            key={country.cca3}
+            flag={country.flags.png}
+            altFlag={country.flags.alt}
+            name={country.name.common}
+            population={country.population.toLocaleString()}
+            region={country.region}
+            capital={country.capital ? country.capital[0] : "No capital"}
+          />
+        </Link>
+        
       ))}
     </section>
   )
