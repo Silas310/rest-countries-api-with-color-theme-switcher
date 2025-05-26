efcd771abd2cc4c8b5c6eed77fb0887b162db6a8
Silas <silascosta310@gmail.com>
2025-05-26 17:42:38 -0300

    feat: add layout change to desktop version

--- src/components/DetailsPage.jsx
+++ src/components/DetailsPage.jsx
@@ -23,14 +23,17 @@ function DetailsPage() {
   
 
   return (
-    <main className="dark:text-light-bg text-light-text p-4 bg-inherit flex flex-col gap-y-8">
-      <nav className="justify-self-start">
+    
+    <main className="dark:text-light-bg text-light-text p-4 sm:p-8 bg-inherit flex 
+    flex-col gap-y-8 [@media(min-width:80rem)]:p-14">
+
+      <nav className="justify-self-start [@media(min-width:80rem)]:mb-6">
         <Link to="/">
           <button className="px-6 box py-1 rounded-sm cursor-pointer"> ← Back</button>
         </Link>
       </nav>
 
-      <section className="flex flex-col gap-y-8">
+      <section className="flex flex-col gap-y-8 [@media(min-width:80rem)]:flex-row justify-between max-w-[90rem]">
         <div className="w-full max-w-xl aspect-[4/3] overflow-hidden">
           <img
             src={country.flags?.png}
@@ -39,37 +42,39 @@ function DetailsPage() {
           />
         </div>
 
-
-        <h1 className="text-2xl font-semibold">{country.name.common}</h1> {/*Country name*/ }
-
-        <div className="flex flex-col gap-y-2">
-          <p><strong className="font-semibold">Native name: </strong> { Object.values(country.name.nativeName).map(n => n.common).join(", ")}</p>
-          <p><strong className="font-semibold">Population: </strong> {country.population.toLocaleString("pt-BR")}</p>
-          <p><strong className="font-semibold">Region: </strong> {country.region}</p>
-          <p><strong className="font-semibold">Sub Region:</strong> {country.subregion}</p>
-          <p><strong className="font-semibold">Capital:</strong> {country.capital}</p>
+        
+        <div className="flex flex-col gap-y-8">
+          <h1 className="text-2xl font-semibold">{country.name.common}</h1> {/*Country name*/ }
+          <div className="flex flex-col gap-y-6 [@media(min-width:80rem)]:flex-row [@media(min-width:80rem)]:gap-x-8">
+            <div className="flex flex-col gap-y-2">
+              <p><strong className="font-semibold">Native name: </strong> { Object.values(country.name.nativeName).map(n => n.common).join(", ")}</p>
+              <p><strong className="font-semibold">Population: </strong> {country.population.toLocaleString("pt-BR")}</p>
+              <p><strong className="font-semibold">Region: </strong> {country.region}</p>
+              <p><strong className="font-semibold">Sub Region:</strong> {country.subregion}</p>
+              <p><strong className="font-semibold">Capital:</strong> {country.capital}</p>
+            </div>
+            <div className="flex flex-col gap-y-2">
+              <p><strong className="font-semibold">Top Level Domain:</strong> {country.tld.join(" - ")}</p>
+              <p><strong className="font-semibold">Currencies:</strong> {Object.values(country.currencies).map(c => c.name).join(", ")}</p>
+              <p><strong className="font-semibold">Languages:</strong> {Object.values(country.languages).join(", ")}</p>
+            </div>
+          </div>
+          
+          <section>
+            <h2>Border countries: </h2>
+            <ul className="flex flex-wrap gap-2 mt-2">
+              {borderCountries.length > 0 ? (
+                borderCountries.map((border) => (
+                  <li key={border} className="box px-2">
+                    {border}
+                  </li>
+                ))
+              ) : (
+                <li className="text-gray-500">No border countries</li>
+              )}
+            </ul>
+          </section>
         </div>
-
-        <div className="flex flex-col gap-y-2">
-          <p><strong className="font-semibold">Top Level Domain:</strong> {country.tld.join(" - ")}</p>
-          <p><strong className="font-semibold">Currencies:</strong> {Object.values(country.currencies).map(c => c.name).join(", ")}</p>
-          <p><strong className="font-semibold">Languages:</strong> {Object.values(country.languages).join(", ")}</p>
-        </div>
-
-        <section>
-          <h2>Border countries: </h2>
-          <ul className="flex flex-wrap gap-2 mt-2">
-            {borderCountries.length > 0 ? (
-              borderCountries.map((border) => (
-                <li key={border} className="box px-2">
-                  {border}
-                </li>
-              ))
-            ) : (
-              <li className="text-gray-500">No border countries</li>
-            )}
-          </ul>
-        </section>
 
       </section>
     </main>
