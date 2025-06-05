ef206a9f6ec6f72250c11464df136d1b0af410e3
Silas <silascosta310@gmail.com>
2025-06-05 10:36:30 -0300

    fix card height

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,7 +1,7 @@
 function Card({flag, altFlag, name, population, region, capital}) {
   return (
     <section className="bg-white dark:bg-dark-element dark:text-white rounded-lg shadow-2xl max-w-[18rem]">
-      <img src={flag} alt={altFlag} className="rounded-t-lg"/>
+      <img src={flag} alt={altFlag} className="min-h-48 w-full object-cover rounded-t-lg"/>
       <div className="p-5 pb-12">
         <h2 className="text-[1.2rem] font-bold mb-3">{name}</h2>
         <p><strong className="font-semibold">Population:</strong> {population}</p>
