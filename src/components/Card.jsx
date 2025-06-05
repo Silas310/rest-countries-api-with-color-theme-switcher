675d2fd0042c7ccc5955fa1abc606663fb14db8f
Silas <silascosta310@gmail.com>
2025-06-05 10:45:41 -0300

    refactor: render Card component using map

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,16 +1,21 @@
-function Card({flag, altFlag, name, population, region, capital}) {
+function Card({ flag, altFlag, name, population, region, capital }) {
+  const info = [
+    { label: 'Population', value: population },
+    { label: 'Region', value: region },
+    { label: 'Capital', value: capital }
+  ];
+
   return (
     <section className="bg-white dark:bg-dark-element dark:text-white rounded-lg shadow-2xl max-w-[18rem]">
-      <img src={flag} alt={altFlag} className="h-48 w-full object-cover rounded-t-lg"/>
+      <img src={flag} alt={altFlag} className="h-48 w-full object-cover rounded-t-lg" />
       <div className="p-5 pb-12">
         <h2 className="text-[1.2rem] font-bold mb-3">{name}</h2>
-        <p><strong className="font-semibold">Population:</strong> {population}</p>
-        <p><strong className="font-semibold">Region:</strong> {region}</p>
-        <p><strong className="font-semibold">Capital:</strong> {capital}</p>
+        {info.map(({ label, value }) => (
+          <p key={label}>
+            <strong className="font-semibold">{label}:</strong> {value}
+          </p>
+        ))}
       </div>
     </section>
-  )
+  );
 }
-
-
-export default Card;
